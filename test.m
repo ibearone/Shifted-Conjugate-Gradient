ea18dc6ce7d7409e5c92e84f24de54bbf63e2d8a
Guanxiong Qu <quguanxiong@gmail.com>
2017-07-17 16:45:12 +0900

    showing sparsity

--- test.m
+++ test.m
@@ -1,41 +1,42 @@
 clc;clear;close all;
 %%% Setting Parameters%%%
-dim=500 %%%dimention of matrix
+dim=1000 %%%dimention of matrix
 %%%%%
- A= diag(randi(1,dim,1))
+ A= diag(randi(10,dim,1))
  R = sprandsym(dim,0.01)
  A=A+R
 
-B=randi(10,dim,1)
+B=randi(1,dim,1)
 
 
-nmax=4
-sigma=[2 1 0.5 0.1 0]
+nmax=6
+sigma=[10 2 1 0.5 0.1 0]
 for n=1:nmax
 [x{n},norm_r{n},norm_p{n}]=Shift_ConG(A,B,sigma(n),0.1^5)
 end
 for n=1:nmax
-subplot(1,2,1)
+subplot(1,3,1)
 plot(norm_r{n},'LineWidth',1)
 hold on;
 end
-legend('\sigma=2','\sigma=1','\sigma=0.5','\sigma=0.1','\sigma=0','location','best')
+legend('\sigma=10','\sigma=2','\sigma=1','\sigma=0.5','\sigma=0.1','\sigma=0','location','best')
 xlabel Steps
 ylabel |r|
 
 set(gca, 'YScale', 'log')
 for n=1:nmax
     
-subplot(1,2,2)
+subplot(1,3,2)
 plot(norm_p{n},'LineWidth',1)
 hold on;
 end
-legend('\sigma=2','\sigma=1','\sigma=0.5','\sigma=0.1','\sigma=0','location','best')
+legend('\sigma=10','\sigma=2','\sigma=1','\sigma=0.5','\sigma=0.1','\sigma=0','location','best')
 xlabel Steps
 ylabel |p|
 
 set(gca, 'YScale', 'log')
-figure;
+
+subplot(1,3,3)
 for n=1:nmax
     X{n}=inv(A+sigma(n)*eye(dim))*B
 for k=1:length(x{n})
@@ -44,8 +45,14 @@ end
 plot(def(:,n),'LineWidth',1)
 hold on;
 end
-legend('\sigma=2','\sigma=1','\sigma=0.5','\sigma=0.1','\sigma=0','location','best')
+legend('\sigma=10','\sigma=2','\sigma=1','\sigma=0.5','\sigma=0.1','\sigma=0','location','best')
+
 xlabel Steps
 ylabel |x-X|
 
 set(gca, 'YScale', 'log')
+figure;
+for n=1:nmax
+    subplot(2,3,n)
+spy(A+sigma(n)*eye(dim))
+end
